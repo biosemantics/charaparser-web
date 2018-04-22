74747ff842196fcb3d4fccad6b0966f0df2bfac9
rodenhausen <thomas.rodenhausen@gmail.com>
2018-04-22 13:30:47 -0700

    initial import

---
+++ src/main/java/edu/arizona/biosemantics/cpservice/CPController.java
@@ -0,0 +1,50 @@
+package edu.arizona.biosemantics.cpservice;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
+import edu.arizona.biosemantics.semanticmarkup.ling.chunk.ChunkCollector;
+import edu.arizona.biosemantics.semanticmarkup.markupelement.description.ling.extract.IDescriptionExtractor;
+import edu.arizona.biosemantics.semanticmarkup.markupelement.description.model.Description;
+import edu.arizona.biosemantics.semanticmarkup.markupelement.description.transform.SentenceChunkerRun;
+
+@RestController
+public class CPController {
+
+	private CPMarkupCreator chunkerRunCreator;
+
+	public CPController() throws Exception {
+		this.chunkerRunCreator = new CPMarkupCreator();
+	}
+
+	@RequestMapping(value = "/parse", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public String parse(@RequestParam String sentence) throws Exception {
+		SentenceChunkerRun chunkerRun = chunkerRunCreator.createChunkerRun(sentence);
+		ChunkCollector chunkCollector = chunkerRun.call();
+		System.out.println(chunkCollector.toString());
+
+		IDescriptionExtractor descriptionExtractor = chunkerRunCreator.createDescriptionExtractor();
+		List<ChunkCollector> chunkCollectors = Arrays.asList(chunkCollector);
+		Description description = new Description();
+		descriptionExtractor.extract(description, 1, chunkCollectors);
+
+		// return description;
+		ObjectMapper mapper = new ObjectMapper();
+		AnnotationIntrospector introspector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+		AnnotationIntrospector secondary = new JacksonAnnotationIntrospector();
+		mapper.setAnnotationIntrospector(new AnnotationIntrospectorPair(introspector, secondary));
+		return mapper.writeValueAsString(description);
+	}
+}
---
+++ src/main/java/edu/arizona/biosemantics/cpservice/CpServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.arizona.biosemantics.cpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpServiceApplication.class, args);
+	}
+}
